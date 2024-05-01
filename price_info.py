52fbae54c1b96152fe52ae037a1512deeb079f12
wangrongyi <WANG06.23@iChat.sp.edu.sg>
2024-05-01 18:41:32 +0800

    complete

--- price_info.py
+++ price_info.py
@@ -4,13 +4,13 @@ price_list={'apple' : 1.20, 'orange':1.40, 'watermelon': 6.50, 'pineapple': 2.70
 quantity_list= {'apple': 5, 'orange':5, 'watermelon': 1, 'pineapple': 2, 'pear' : 10, 'papaya': 1, 'pomegranate': 2}
 
 
-def total_cost_shopping():
+def total_cost_shopping(price_list,quantity_list):
     total_cost = 0
-    for key in price_list.keys():
-        if key in quantity_list:
-            # complete the implementation below:
-
-    print("total cost = ", total_cost)
+    for fruit, quantity in quantity_list.items():
+        if fruit in price_list:
+            total_cost += quantity * price_list[fruit]
+    print("Total cost =", total_cost)
+   
 
 
 def cost_of_fruits(fruit, quantity):
@@ -25,7 +25,7 @@ def cost_of_fruits(fruit, quantity):
 def main():
 
     cost_of_fruits('apple', 10)
-    total_cost_shopping()
+    total_cost_shopping(price_list,quantity_list)
 
 
 if __name__ == "__main__":
